1de361749a1b004f5699eca865b0dab54c7d5393
wangjydp <wangjy@dp.tech>
2024-05-14 18:07:52 +0800

    new version

--- strain.py
+++ strain.py
@@ -21,7 +21,7 @@ from util.SPcalc import System as syssp
 from util.Align import Align as align
 from util.CalRMSD import RMSD
 from util.ConfRelaxbySQM import System as MDsample
-from util.Cluster import cluster
+#from util.Cluster import cluster
 
 
 class main():
@@ -34,7 +34,7 @@ class main():
         self.main_dir = os.getcwd()
         
         ## initial
-        _dic_rotableBond = {0: 100, 1: 300}
+        _dic_rotableBond = {0: 80, 1: 150} ## used to be 100, 300
         #_dic_HA_index = {}
         try:
             self.get_mol = [mm for mm in Chem.SDMolSupplier(self.db_name) if mm][0]
@@ -68,11 +68,14 @@ class main():
         except Exception as e:
             self.charge = None
         
-        
+
+        """
         try:
             self.rmsd_cutoff_initial_opt = float(args["rmsd_cutoff_initial_opt"])
         except Exception as e:
             self.rmsd_cutoff_initial_opt = 0.5
+        
+        """
         
         #print(f"gen conformer: {self.N_gen_conformer}")
         
@@ -80,6 +83,7 @@ class main():
         #self.if_csv = args["if_csv"]  
 
         ##parameter for sample & cluster
+        """
         try:
             self.k = args["cluster_n"]
         except Exception as e:
@@ -87,7 +91,9 @@ class main():
         else:
             if not isinstance(self.k, int):
                 self.k = None
+        """
 
+        
         try:
             self.distance_cutoff = args["rmsd_cutoff_cluster"]
         except Exception as e:
@@ -97,24 +103,40 @@ class main():
                 self.distance_cutoff = 0.85
         
         try:
-            self.sp_max_n = args["n_conformer_for_sp"]
+            self.energy_cutoff = args["energy_cutoff"]
+        except Exception as e:
+            self.energy_cutoff = 0.5
+        else:
+            if not isinstance(self.energy_cutoff, float):
+                self.energy_cutoff = 0.5
+        
+        try:
+            self.sp_max_n = int(args["n_conformer_for_sp"])
         except Exception as e:
             self.sp_max_n = 1
-        
+               
+        try:
+            self.verbose = args["verbose"]
+        except Exception as e:
+            self.verbose = False
+
         self.prefix = ".".join(self.db_name.split(".")[:-1])
       
     
     def step1_initial_opt(self):
         ## try without HeavyAtom constrain first
-        opt = []
+        #opt = []
 
         if self.charge != None:
             _initial_opt = sysopt(input_sdf=self.db_name, 
-                                define_charge=self.charge).run()
+                                define_charge=self.charge,
+                                HA_constrain=True).run()
         else:
-            _initial_opt = sysopt(input_sdf=self.db_name).run()
+            _initial_opt = sysopt(input_sdf=self.db_name,
+                                  HA_constrain=True).run()
             self.charge = _initial_opt[0].GetProp("charge")
         
+        """
         get_aligned_search = align(SearchMolObj=_initial_opt[0], RefMolObj=self.get_mol, method="crippen3D").run()
         if get_aligned_search:
             get_rmsd = RMSD(rdmolobj_mol=_initial_opt[0], rdmolobj_ref=self.get_mol, method="selfWhole").run()
@@ -126,16 +148,19 @@ class main():
             opt = sysopt(input_sdf=self.db_name,
                             HA_constrain=True, 
                             define_charge=self.charge).run()
+
         else:
             logging.info("Initial opt met rmsd requirement, no constrain performed")
             opt = _initial_opt
+        """
         
-        if not opt:
+        #if not opt:
+        if not _initial_opt:
             logging.info("Failed at initial opt")
             return None
         
         cc = Chem.SDWriter(f"{self.prefix}.initial_opt.sdf")
-        cc.write(opt[0])
+        cc.write(_initial_opt[0])
         cc.close()
 
         return f"{self.prefix}.initial_opt.sdf"
@@ -154,12 +179,13 @@ class main():
 
         return sampled_file_name
     
+    """
     def step3_cluster(self, input_sdf_name):
 
-        filtered_mol = cluster(input_sdf=input_sdf_name,
-                cluster_n=self.k,
-                rmsd_cutoff_cluster=self.distance_cutoff,
-                if_write_cluster_mol=False).run()
+        #filtered_mol = cluster(input_sdf=input_sdf_name,
+        #        cluster_n=self.k,
+        #        rmsd_cutoff_cluster=self.distance_cutoff,
+        #        if_write_cluster_mol=False).run()
         
         #filtered_file_name = [cc for cc in os.listdir() if ("FILTER_" in cc) and (cc.endswith(".sdf"))]
         if not filtered_mol:
@@ -172,14 +198,15 @@ class main():
         cc.close()
 
         return "FILTER.sdf"
+    """
     
-    def step4_opt(self, input_sdf_name):
+    def step3_opt(self, input_sdf_name):
 
         tracker = []
         
         opted = sysopt(input_sdf=input_sdf_name, 
                        rmsd_cutoff=self.distance_cutoff,
-                       save_n=self.sp_max_n,
+                      # save_n=self.sp_max_n,
                        HA_constrain=False, 
                        define_charge=self.charge,
                        if_write_sdf=False).run()
@@ -188,15 +215,23 @@ class main():
             logging.info("Failed at optimization")
             return None
 
-        for idx, mm in enumerate(opted):
-            cc = Chem.SDWriter(f"OPT_{idx}.sdf")
-            cc.write(mm)
-            cc.close()
-            tracker.append(f"OPT_{idx}.sdf")
+        sorted_opt = sorted(opted, key=lambda x: float(x.GetProp("Energy_xtb")))
+        i = 1
+        saved = sorted_opt[:1]
+
+        while i < len(sorted_opt):
+
+            energy_diff = [abs(float(saved[ii].GetProp("Energy_xtb"))- float(sorted_opt[i].GetProp("Energy_xtb"))) * 627.51
+                            for ii in range(len(saved))]
+
+            if min(energy_diff) > self.energy_cutoff:
+                saved.append(sorted_opt[i])
+
+            i += 1
         
-        return tracker
+        return saved
     
-    def step5_sp(self, tracker):
+    def step4_sp(self, mol_set):
         ## decide whether carry out sp calc
 
         if self.special_type:
@@ -211,60 +246,69 @@ class main():
         mol_initial_opt = [cc for cc in Chem.SDMolSupplier(f"{self.prefix}.initial_opt.sdf", removeHs=False) if cc]
         xtb_initial_opt = float(mol_initial_opt[0].GetProp("Energy_xtb"))
 
-        opted_mol = {}
+        pose_with_energy_label_initial_opt, _ = syssp(input_rdmol_obj=mol_initial_opt, 
+                                                charge_method="define", 
+                                                define_charge=self.charge, 
+                                                basis=applied_basis).run_pyscf()
+        
+        mol_initial_opt[0].SetProp("Energy_dft", f"{pose_with_energy_label_initial_opt[0][0]}")
+        starter = Chem.SDWriter(f"{self.prefix}.initial_opt.sdf")
+        starter.write(mol_initial_opt[0])
+        starter.close()
+
+        self.sp_max_n = min(len(mol_set), self.sp_max_n)
 
         flag = 0
 
-        for idx, name in enumerate(tracker):
-            get_mol =  [cc for cc in Chem.SDMolSupplier(name, removeHs=False) if cc]
-            get_energy = (float(get_mol[0].GetProp("Energy_xtb")) - xtb_initial_opt) * 627.51
-            if get_energy < flag:
-                flag = get_energy
-            opted_mol.setdefault(name, get_mol)
+        sp_dic = {}
         
-        if flag == 0:
-            ## no need for sp calc
+        for ii, each_mol in enumerate(mol_set[:self.sp_max_n]):
+            pose_with_energy_label, _ = syssp(input_rdmol_obj=[each_mol], 
+                                                charge_method="define", 
+                                                define_charge=self.charge, 
+                                                basis=applied_basis).run_pyscf()
+
+            each_mol.SetProp("Energy_dft", f"{pose_with_energy_label[0][0]}")
+            ee_strain = (pose_with_energy_label[0][0] - pose_with_energy_label_initial_opt[0][0]) * 627.51
+
+            sp_dic.setdefault(ii, [each_mol, ee_strain])
+        
+        get_GM = sorted(sp_dic.items(), key=lambda x: x[1][-1])[0]
+        
+        if get_GM[-1][-1] > 0:
             GM = mol_initial_opt
             GM_energy = "~ 1"
-            write_basis = None
         else:
-            sp_dic = {}
-            pose_with_energy_label_initial_opt, _ = syssp(input_rdmol_obj=mol_initial_opt, 
-                                                 charge_method="define", 
-                                                 define_charge=self.charge, 
-                                                 basis=applied_basis).run_pyscf()
-            
-            mol_initial_opt[0].SetProp("Energy_dft", f"{pose_with_energy_label_initial_opt[0][0]}")
+            GM = get_GM[-1][0]
+            GM_energy = abs(get_GM[-1][-1])
 
-            for kk, vv in opted_mol.items():
-                pose_with_energy_label, _ = syssp(input_rdmol_obj=vv, 
-                                                 charge_method="define", 
-                                                 define_charge=self.charge, 
-                                                 basis=applied_basis).run_pyscf()
-                
-                vv[0].SetProp("Energy_dft", f"{pose_with_energy_label[0][0]}")
-                ee_strain = (pose_with_energy_label[0][0] - pose_with_energy_label_initial_opt[0][0]) * 627.51
-
-                sp_dic.setdefault(kk, [vv, ee_strain])
-            
-            get_GM = sorted(sp_dic.items(), key=lambda x: x[1][-1])[0]
-            
-            if get_GM[-1][-1] > 0:
-                GM = mol_initial_opt
-                GM_energy = "~ 1"
-            else:
-                GM = get_GM[-1][0]
-                GM_energy = abs(get_GM[-1][-1])
-
-        cc = Chem.SDWriter(f"Stable_{self.prefix}.sdf")
+        cc = Chem.SDWriter("Global.sdf")
         cc.write(GM[0])
         cc.close()
+        logging.info("Global minima conformation has been saved in [Global.sdf]")
+        
+
+        if self.sp_max_n > 1:
+            more_cc = Chem.SDWriter(f"AppendixConf_top{self.sp_max_n}.sdf")
+            for each in sorted(sp_dic.items(), key=lambda x: x[1][-1]):
+                more_cc.write(each[0])
+            more_cc.close()
+            logging.info(f"All conformations with DFT level energy labels were saved in [AppendixConf_top{self.sp_max_n}.sdf]")
+
+        if self.verbose:
+            more_and_more_cc = Chem.SDWriter(f"AppendixConf_rest.sdf")
+            for mol in mol_set[self.sp_max_n:]:
+                more_and_more_cc.write(mol)
+            more_and_more_cc.close()
+            logging.info(f"More conformations without DFT energy labels were saved in [AppendixConf_rest.sdf]")
 
         df = pd.DataFrame({"LigandName": [self.prefix],
                             "Applied Basis": [applied_basis],
                             "Strain Energy/kcal.mol-1": [f"{GM_energy}"]})
 
+
         df.to_csv("RESULT.STRAIN.csv", index=None)
+        logging.info("Recorded strain energy saved in [RESULT.STRAIN.csv]")
 
         #os.system(f"rm -f OPT_*.sdf SAVE.sdf FILTER.sdf")
 
@@ -279,7 +323,8 @@ class main():
         if not os.path.exists(work_dir):
             os.mkdir(work_dir)
         os.chdir(work_dir)
-        os.system(f"mv {self.main_dir}/{self.db_name} ./")
+        target = os.path.join(self.main_dir, self.db_name)
+        os.system(f"mv {target} {work_dir}")
 
         ## step1
         logging.info("Step1: Performing initial opt ....")
@@ -297,23 +342,22 @@ class main():
             logging.info("Terminated at Step2.")
             return 
         
-        logging.info("Step3: Clustering ...")
-        self.filter_name = self.step3_cluster(self.sampled_file)
-        if not self.filter_name:
-            logging.info("Terminated at Step3.")
-            return
+        #logging.info("Step3: Clustering ...")
+        #self.filter_name = self.step3_cluster(self.sampled_file)
+        #if not self.filter_name:
+        #    logging.info("Terminated at Step3.")
+        #    return
         
-        logging.info("Step4: Optimization ...")
-        self.opted_name_list = self.step4_opt(self.filter_name)
-        if not self.opted_name_list:
+        logging.info("Step3: Optimization ...")
+        self.opted_mol = self.step3_opt(self.sampled_file)
+        if not self.opted_mol:
             logging.info("Terminated at Step4.")
         
-        logging.info("Step5: Energy calculation ...")
-        result_file_tag = self.step5_sp(self.opted_name_list)
+        logging.info("Step4: Energy calculation ...")
+    
+        result_file_tag = self.step4_sp(self.opted_mol)
         if result_file_tag:
             logging.info("Finish strain calculation")
-            logging.info("Stable pose save in Stable_*.sdf")
-            logging.info("Others in *.csv")
         
         logging.info("Done")
         
@@ -331,33 +375,35 @@ if __name__ == '__main__':
                         help='input sdf file, docking pose(s) for single mol')
     parser.add_argument('--N_gen_conformer', type=int, 
                         help='available for [denovo and sample] mode, define N conformers in sampling stage, \
-                        if not defined, 100 if rotable bond <=5, else 300')
+                        if not defined, 80 if rotable bond <=5, else 150')
     parser.add_argument('--define_charge', type=str, default=None,
                         help='define charge for input, use you sense that rdkit may fail to get corret bond order, default None')
-    parser.add_argument('--rmsd_cutoff_initial_opt', type=float,
-                        help='define rmsd cutoff for initial opt, default is 0.5')
+    #parser.add_argument('--rmsd_cutoff_initial_opt', type=float,
+    #                    help='define rmsd cutoff for initial opt, default is 0.5')
     #parser.add_argument('--not_save_csv', default=True, action='store_false', \
     #                    help="adding this option will not save final result in csv file")
-    parser.add_argument('--cluster_n', type=int, 
-                        help='set static n for clustering after sampling')
-    parser.add_argument('--rmsd_cutoff', type=float,
+    #parser.add_argument('--cluster_n', type=int, 
+    #                    help='set static n for clustering after sampling')
+    parser.add_argument('--rmsd_cutoff', type=float, default=0.85,
                         help='rmsd cutoff for reduce redundancy, default 0.85')
-    parser.add_argument('--n_conformer_for_sp', type=int,
+    parser.add_argument('--n_conformer_for_sp', type=int, default=1,
                         help='define the maximum conformers involved in final dft calculation, default 1')
+    parser.add_argument('--energy_cutoff', type=float, default=0.5,
+                        help='energy cutoff to save pose for single point calculation, unit in kcal/mol, default 0.5')
+    parser.add_argument('--verbose', default=False, action='store_true',
+                        help='if save more conformation for visual check after single point energy calculation, default False')
+    
     #parser.add_argument('--no_constrain', default=True, action='store_false', \
     #                    help="adding this option will turnoff constrain on heavy atoms when performing geometry optimization")
     args = parser.parse_args()
 
-    main(input_sdf=args.input_sdf, \
-         N_gen_conformer=args.N_gen_conformer, \
-         define_charge=args.define_charge, \
-         rmsd_cutoff_initial_opt=args.rmsd_cutoff_initial_opt,\
-         clsuter_n=args.cluster_n, \
-         rmsd_cutoff_cluster=args.rmsd_cutoff, \
-         n_conformer_for_sp=args.n_conformer_for_sp).run()
-
-
-
+    main(input_sdf=args.input_sdf, 
+         N_gen_conformer=args.N_gen_conformer, 
+         define_charge=args.define_charge, 
+         rmsd_cutoff_cluster=args.rmsd_cutoff,
+         n_conformer_for_sp=args.n_conformer_for_sp,
+         energy_cutoff=args.energy_cutoff,
+         verbose=args.verbose).run()
 
     
 ##
